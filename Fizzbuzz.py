d30d618b2e2ba26060ea7f3e87f0b99f874a21ad
Fatima Sheikhnur <fsheikhnur@spartaglobal.com>
2023-04-04 16:26:08 +0100

    fizzbuzz and moveratings with functions

---
+++ Fizzbuzz.py
@@ -0,0 +1,13 @@
+# FizzBuzz - Write a program that prints numbers from 1 - 100 but for multiples of 3 print "Fizz" and for multiples
+# of 5 print "Buzz". For multiples of 3 and 5 print "FizzBuzz
+
+num = 1
+
+while num <= 100:
+    if num % 15 == 0:
+        print("FizzBuzz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
+    num += 1
--- movie_ratings.py
+++ movie_ratings.py
@@ -1,16 +1,15 @@
 user_age = input("Enter your age:  ")
 
 age = int(user_age)
-if age >= 18: # User is 18+
+
+if age >= 18:  # User is 18+
     print("The following movie ratings are available for you: U, PG, 12, 15, 18")
-elif age >= 15: # User is 15 +
+elif age >= 15:  # User is 15 +
     print("The following movie ratings are available for you: U, PG, 12, 15")
-elif age < 12: # User is younger than 12
+elif age < 12:  # User is younger than 12
     print("The following movie ratings are available for you: U ")
-elif age >= 12: # User is 12+
+elif age >= 12:  # User is 12+
     print("The following movie ratings are available for you: U, PG, 12")
 else:
     print("This is not a valid age")
-
-
 
